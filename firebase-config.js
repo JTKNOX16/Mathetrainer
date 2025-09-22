af18e0cca7a94f63cd0228b54de63f4709d035a2
JTKNOX16 <konradpost@gmail.com>
2025-09-22 19:41:02 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCWlIj731UkNOgoNfkvYQX2a2yXVS2Lg0k",
-  authDomain: "math-trainer-b4e77.firebaseapp.com",
-  projectId: "math-trainer-b4e77",
-  storageBucket: "math-trainer-b4e77.firebasestorage.app",
-  messagingSenderId: "48449632113",
-  appId: "1:48449632113:web:8912f7ed68bb3054a368d0"
-};
-export const DEFAULT_GROUP_ID = "3a-2025";
